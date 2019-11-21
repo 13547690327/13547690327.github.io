2454021b95e83b77eb38233ce82c9c347fa4db6e
13547690327 <33546903+13547690327@users.noreply.github.com>
2019-11-21 22:16:14 +0800

    new

---
+++ demo/demo13/precache-manifest.233b6aed7db0d8d8a934c270031b8eed.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7bd783be9ffae34cb6",
+    "url": "/css/app.a6068b3b.css"
+  },
+  {
+    "revision": "0bda055680b6336a6aeb",
+    "url": "/css/chunk-717d794c.7fcb7f0d.css"
+  },
+  {
+    "revision": "b2ce09a830223fa0ceab",
+    "url": "/css/chunk-7928898a.17e436eb.css"
+  },
+  {
+    "revision": "48e14387c39cd747fcd63bb34fa75a80",
+    "url": "/img/0.48e14387.png"
+  },
+  {
+    "revision": "2ea9b18fa285432f9050a59bdfbd1301",
+    "url": "/img/1.2ea9b18f.jpg"
+  },
+  {
+    "revision": "db475701096458175d4d3e36f2754968",
+    "url": "/img/2.db475701.jpg"
+  },
+  {
+    "revision": "fe18d06ed3d751532575899037c05847",
+    "url": "/img/3.fe18d06e.jpg"
+  },
+  {
+    "revision": "b05fe242273559073e60c7f5b3fe80c6",
+    "url": "/img/4.b05fe242.png"
+  },
+  {
+    "revision": "e3b89573a7a5ba53be596a5d2323a495",
+    "url": "/img/4.e3b89573.jpg"
+  },
+  {
+    "revision": "68247ed3faa086a92914eae7640fcb17",
+    "url": "/img/5.68247ed3.png"
+  },
+  {
+    "revision": "3224e2dda01dc446c4a4ccf814df10ed",
+    "url": "/img/6.3224e2dd.png"
+  },
+  {
+    "revision": "dda78cf50079bf476200541446cb323c",
+    "url": "/img/iconfont.dda78cf5.svg"
+  },
+  {
+    "revision": "e64c2f981582f929b2721d30584437bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c7bd783be9ffae34cb6",
+    "url": "/js/app.7f9e569e.js"
+  },
+  {
+    "revision": "0bda055680b6336a6aeb",
+    "url": "/js/chunk-717d794c.ee0f7424.js"
+  },
+  {
+    "revision": "b2ce09a830223fa0ceab",
+    "url": "/js/chunk-7928898a.beb7aa13.js"
+  },
+  {
+    "revision": "8eded8c1c2fb723fe4e5",
+    "url": "/js/chunk-vendors.52113e84.js"
+  },
+  {
+    "revision": "bfa070c4db257ca2c73458dc29de879d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
